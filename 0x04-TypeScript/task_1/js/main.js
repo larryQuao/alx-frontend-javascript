fe69ca025b0e4597288eb4f1212d117ffccc73ca
larryQuao <honyadjilawrence2s1@gmail.com>
2025-09-16 00:55:57 +0000

    chore: update main.ts and main.js

--- 0x04-TypeScript/task_1/js/main.js
+++ 0x04-TypeScript/task_1/js/main.js
@@ -14,3 +14,11 @@ var director1 = {
 };
 console.log(typeof teacher1, teacher1);
 console.log(typeof director1, director1);
+var printTeacher = function (firstName, lastName) {
+    return "".concat(firstName.charAt(0), ".").concat(lastName);
+};
+// function printTeacher(firstName: string, lastName: string): string {
+//     return `${firstName.charAt(0)}.${lastName}`;
+// };
+console.log(printTeacher("John", "Doe")); // Output: J.Doe
+console.log(printTeacher("Jane", "Smith")); // Output: J.Smith
--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,4 +1,26 @@
-interface Directors extends Teacher {
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomework(): string {
+        return "Currently working";
+    }
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
+interface Director extends Teacher {
     readonly numberOfReports: number;
 }
 
@@ -24,7 +46,7 @@ const teacher1: Teacher = {
     contract: false
 }
 
-const director1: Directors = {
+const director1: Director = {
     firstName: "Alice",
     lastName: "Johnson",
     fullTimeEmployee: true,
@@ -35,10 +57,13 @@ const director1: Directors = {
 console.log(typeof teacher1, teacher1);
 console.log(typeof director1, director1);
 
-const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+const printTeacher: printTeacherFunction = (firstName, lastName): string => {
     return `${firstName.charAt(0)}.${lastName}`;
 }
 
 // function printTeacher(firstName: string, lastName: string): string {
 //     return `${firstName.charAt(0)}.${lastName}`;
 // };
+
+console.log(printTeacher("John", "Doe"));  // Output: J.Doe
+console.log(printTeacher("Jane", "Smith")); // Output: J.Smith
